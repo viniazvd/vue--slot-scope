3c89f4dc777139274c1e314c3d6a1f1efec9be1e
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2018-03-21 09:38:54 -0300

    new example added

--- src/components/Pai.vue
+++ src/components/Pai.vue
@@ -15,6 +15,7 @@
       <div slot="conteudo" slot-scope="{ text }">
         {{ text }}
       </div>
+      <div slot="footer">footer</div>
     </filho1>
 
     <filho2>
@@ -22,15 +23,26 @@
         {{ props.text }}
       </li>
     </filho2>
+
+    <table>
+      <table-row :row="['Vitor', 'Vinicius', 'Guilherme']">
+        <td slot="cell-2" slot-scope="{ cell, key }">
+          {{ cell }}
+          <Button icon="&lt;">Selecionar o {{ cell }}</Button>
+        </td>
+      </table-row>
+    </table>
   </div>
 </template>
 
 <script>
 import filho1 from './Filho1.vue';
 import filho2 from './Filho2.vue';
+import Button from './Button';
+import TableRow from './TableRow';
 
 export default {
-  components: { filho1, filho2 }
+  components: { filho1, filho2, Button, TableRow }
 }
 </script>
 
