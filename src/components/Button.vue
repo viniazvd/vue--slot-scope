85175e241dfddcc47ab3f7cbdb248d10b06d825a
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2018-03-21 09:37:31 -0300

    new example

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button :class="classes">
+    <span>{{ icon }}</span>
+
+    <span v-if="!!$slots.default" class="text">
+      <slot />
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: String,
+  },
+  computed: {
+    classes () {
+      return ['Button', { '-empty': !!this.$slots.default }]
+    }
+  }
+}
+</script>
+
---
+++ src/components/TableRow.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>TESTE SLOT-SCOPE $SLOTS</h2>
+
+    <slot
+      v-for="(cell, index) in row"
+      :name="'cell-' + index"
+      :cell="cell"
+      :index="index">
+
+      <tr :key="index">
+        <td :key="index">{{ cell }}</td>
+      </tr>
+    </slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      row: Array
+    }
+  }
+</script>
+
